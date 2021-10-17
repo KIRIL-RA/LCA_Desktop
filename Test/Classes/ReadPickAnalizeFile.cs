7539b4ff9c1a1565e93dc67c09ff6faf51e4fdf2
KIRILL-RA <kirill0627205@gmail.com>
2021-10-17 16:48:33 +0300

    Добавьте файлы проекта.

---
+++ Test/Classes/ReadPickAnalizeFile.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System;
+
+namespace Test.Classes
+{
+    struct Analized_line
+    {
+        /*
+         * structure containing data from one record
+         */
+
+        public string Date;
+        public int Machine_state;
+        public int Laser_state;
+    }
+
+    class ReadPickAnalizeFile
+    {
+        /*
+         * ReadPickFile class
+         * Class containing functions for opening, reading, parse and analize log files
+         */
+
+        public OpenFileDialog openFileDialogCSV;
+
+        public string fileName = null;
+        public string write_date = null;
+
+        public string[] lines = null;
+        public string[] write_dates = null;
+
+        public int lines_count = 0;
+
+        TimeSpan write_time;
+        public Analized_line[] analized_file = null;
+
+        public ReadPickAnalizeFile()
+        {
+            openFileDialogCSV = new OpenFileDialog();
+
+            openFileDialogCSV.InitialDirectory = "c:\\";
+            openFileDialogCSV.Filter = "Log files |*.csv";
+        }
+
+        public bool? Pick_File()
+        {
+            /* 
+             * Funtion for open dialog to choose log file
+             */
+
+            Trace.WriteLine("-- Open file dialog");
+            bool? isFileOpened = openFileDialogCSV.ShowDialog();
+            fileName = openFileDialogCSV.FileName;
+
+            return isFileOpened;
+        }
+
+        public bool Read_File()
+        {
+            /*
+             * Function for reading choosed file
+             */
+
+            if (fileName != null)
+            {
+                lines = System.IO.File.ReadAllLines(fileName);
+                lines_count = lines.Length;
+                Trace.WriteLine("-- Readed " + lines_count + " lines");
+
+                return true;
+            }
+            else
+            {
+                Trace.WriteLine("-- File not readed");
+                return false;
+            }
+        }
+
+        public bool Parse_File()
+        {
+            if (lines != null)
+            {
+                Trace.WriteLine("-- Parse file...");
+                analized_file = new Analized_line[lines_count];
+
+                for (int line = 0; line < lines_count; line++)
+                {
+                    string[] data = lines[line].Split(',');
+
+                    analized_file[line].Date = data[0];
+                    try
+                    {
+                        analized_file[line].Machine_state = Int32.Parse(data[1]);
+                        analized_file[line].Laser_state = Int32.Parse(data[2]);
+                    }
+                    catch(FormatException e)
+                    {
+                        Trace.WriteLine("-- Error parse: " + e.Message);
+                        return false;
+                    }
+                }
+
+                
+                Trace.WriteLine("-- Parsed " + analized_file.Length + " lines");
+                return true;
+            }
+            else
+            {
+                Trace.WriteLine("--Parse not started");
+                return false;
+            }
+        }
+
+        public void Analize()
+        {
+            write_dates = new string[2] {analized_file[0].Date,  analized_file[analized_file.Length-1].Date};
+            write_date = write_dates[0] + " - " + write_dates[1];
+            Trace.WriteLine("-- Log dates: " + write_date);
+
+            write_time = TimeSpan.FromSeconds(lines_count);
+            Trace.WriteLine("-- Write time: " + write_time);
+        }
+    }
+}
---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.IO;
+using Test.Classes;
+using System.Diagnostics;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ReadPickAnalizeFile csvPickedFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            csvPickedFile = new ReadPickAnalizeFile();
+        }
+
+        private void Select_file()
+        {
+            bool? isOpened = csvPickedFile.Pick_File();
+
+            if (isOpened == true)
+            {
+                Trace.WriteLine("-- File choosed succesful: " + csvPickedFile.fileName);
+
+                File_not_selected1.Visibility = Visibility.Hidden;
+                File_not_selected.Visibility = Visibility.Hidden;
+                File_selected.Visibility = Visibility.Visible;
+                File_name.Content = csvPickedFile.fileName;
+
+                csvPickedFile.Read_File();
+                csvPickedFile.Parse_File();
+                csvPickedFile.Analize();
+
+                Write_date.Content = csvPickedFile.write_date;
+                Main_analize_page.Visibility = Visibility.Visible;
+            }
+            else Trace.WriteLine("-- File not choosed");
+
+        }
+
+        private void PickFileInfoMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Select_file();
+        }
+
+        private void PickFileStartPage_Click(object sender, RoutedEventArgs e)
+        {
+            Select_file();
+        }
+    }
+}
