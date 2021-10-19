c780bc10a5959a36c8353d939e18c65aeacf9715
KIRILL-RA <kirill0627205@gmail.com>
2021-10-19 21:19:21 +0300

    Ver1.0

--- Test/Classes/ReadPickAnalizeFile.cs
+++ Test/Classes/ReadPickAnalizeFile.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace Test.Classes
 {
-    struct Analized_line
+    struct One_record
     {
         /*
          * structure containing data from one record
@@ -22,104 +22,150 @@ namespace Test.Classes
          * Class containing functions for opening, reading, parse and analize log files
          */
 
-        public OpenFileDialog openFileDialogCSV;
-
-        public string fileName = null;
         public string write_date = null;
 
         public string[] lines = null;
         public string[] write_dates = null;
 
         public int lines_count = 0;
+        public int work_time_percent = 0;
+        public int laser_work_time_percent = 0;
+        private int work_time_ = 0;
+        private int laser_work_time_ = 0;
 
-        TimeSpan write_time;
-        public Analized_line[] analized_file = null;
+        public TimeSpan write_time;
+        public TimeSpan work_time;
+        public TimeSpan laser_work_time;
+        public One_record[] graph_work_hours;
+        private One_record[] analized_file = null;
 
         public ReadPickAnalizeFile()
         {
-            openFileDialogCSV = new OpenFileDialog();
+            
 
-            openFileDialogCSV.InitialDirectory = "c:\\";
-            openFileDialogCSV.Filter = "Log files |*.csv";
+            
         }
 
-        public bool? Pick_File()
+        public static string Pick_File()
         {
             /* 
              * Funtion for open dialog to choose log file
              */
+            
+            OpenFileDialog openFileDialogCSV = new OpenFileDialog();
+
+            openFileDialogCSV.InitialDirectory = "c:\\";
+            openFileDialogCSV.Filter = "Log files |*.csv";
 
             Trace.WriteLine("-- Open file dialog");
             bool? isFileOpened = openFileDialogCSV.ShowDialog();
-            fileName = openFileDialogCSV.FileName;
 
-            return isFileOpened;
+            if(isFileOpened == true) return openFileDialogCSV.FileName;
+            return null;
         }
 
-        public bool Read_File()
+        public bool Read_File(string fileName)
         {
             /*
              * Function for reading choosed file
              */
+            lines = System.IO.File.ReadAllLines(fileName);
+            lines_count = lines.Length;
+            Trace.WriteLine("-- Readed " + lines_count + " lines");
 
-            if (fileName != null)
-            {
-                lines = System.IO.File.ReadAllLines(fileName);
-                lines_count = lines.Length;
-                Trace.WriteLine("-- Readed " + lines_count + " lines");
-
-                return true;
-            }
-            else
-            {
-                Trace.WriteLine("-- File not readed");
-                return false;
-            }
+            return true;
         }
 
         public bool Parse_File()
         {
-            if (lines != null)
-            {
-                Trace.WriteLine("-- Parse file...");
-                analized_file = new Analized_line[lines_count];
+            /*
+             * Function to find dates and states in a file
+             */
+            Trace.WriteLine("-- Parse file...");
+            analized_file = new One_record[lines_count];
 
-                for (int line = 0; line < lines_count; line++)
+            for (int line = 0; line < lines_count; line++)
+            {
+                string[] data = lines[line].Split(',');
+
+                analized_file[line].Date = data[0];
+                try
                 {
-                    string[] data = lines[line].Split(',');
-
-                    analized_file[line].Date = data[0];
-                    try
-                    {
-                        analized_file[line].Machine_state = Int32.Parse(data[1]);
-                        analized_file[line].Laser_state = Int32.Parse(data[2]);
-                    }
-                    catch(FormatException e)
-                    {
-                        Trace.WriteLine("-- Error parse: " + e.Message);
-                        return false;
-                    }
+                    analized_file[line].Machine_state = Int32.Parse(data[1]);
+                    analized_file[line].Laser_state = Int32.Parse(data[2]);
                 }
+                catch (FormatException e)
+                {
+                    Trace.WriteLine("-- Error parse: " + e.Message);
+                    return false;
+                }
+            }
 
-                
-                Trace.WriteLine("-- Parsed " + analized_file.Length + " lines");
-                return true;
-            }
-            else
-            {
-                Trace.WriteLine("--Parse not started");
-                return false;
-            }
+
+            Trace.WriteLine("-- Parsed " + analized_file.Length + " lines");
+            return true;
         }
 
-        public void Analize()
+        public bool Analize()
         {
-            write_dates = new string[2] {analized_file[0].Date,  analized_file[analized_file.Length-1].Date};
-            write_date = write_dates[0] + " - " + write_dates[1];
-            Trace.WriteLine("-- Log dates: " + write_date);
+            /*
+             * Function for find write date, write time, work time, laser work time
+             */
+            try
+            {
+                write_dates = new string[2] { analized_file[0].Date, analized_file[analized_file.Length - 1].Date };
+                write_date = write_dates[0] + " - " + write_dates[1];
+                Trace.WriteLine("-- Log dates: " + write_date);
 
-            write_time = TimeSpan.FromSeconds(lines_count);
-            Trace.WriteLine("-- Write time: " + write_time);
+                write_time = TimeSpan.FromSeconds(lines_count);
+                Trace.WriteLine("-- Write time: " + write_time);
+
+                laser_work_time_ = work_time_ = 0;
+
+                foreach (One_record line in analized_file)
+                {
+                    if (line.Laser_state == 1) laser_work_time_++;
+                    if (line.Machine_state == 1) work_time_++;
+                }
+
+                work_time = TimeSpan.FromSeconds(work_time_);
+                laser_work_time = TimeSpan.FromSeconds(laser_work_time_);
+
+                work_time_percent = (int)Math.Round((double)((double)work_time_ / (double)lines_count) * 100);
+                laser_work_time_percent = (int)Math.Round((double)((double)laser_work_time_ / (double)lines_count) * 100);
+
+                graph_work_hours = new One_record[write_time.Hours];
+                for (int hour = 0; hour < graph_work_hours.Length; hour++)
+                {
+                    int laser_work = 0;
+                    int machine_work = 0;
+
+                    // 3600 - 1 hour in second. We iterate over all records by the hour to determine the activity for each hour
+                    for (int second = hour * 3600; second < (hour + 1) * 3600; second++)
+                    {
+                        if (analized_file[second].Laser_state == 1) laser_work++;
+                        if (analized_file[second].Machine_state == 1) machine_work++;
+                    }
+
+                    graph_work_hours[hour].Laser_state = (int)Math.Round((double)((double)laser_work / 3600) * 100);
+                    graph_work_hours[hour].Machine_state = (int)Math.Round((double)((double)machine_work / 3600) * 100);
+                    graph_work_hours[hour].Date = hour.ToString();
+                }
+
+                Trace.WriteLine("-- Work time: " + work_time + " (" + work_time_percent + ')');
+                Trace.WriteLine("-- Laser work time: " + laser_work_time + " (" + laser_work_time_percent + ')');
+                Trace.WriteLine("-- Work in hours");
+                foreach (One_record hour in graph_work_hours)
+                {
+                    Trace.WriteLine("--- Time: " + hour.Date + " -Laser: " + hour.Laser_state + "-Work: " + hour.Machine_state);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Windows;
 using System.IO;
+using LiveCharts;
+using LiveCharts.Wpf;
 using Test.Classes;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Test
 {
@@ -19,38 +22,87 @@ namespace Test
             csvPickedFile = new ReadPickAnalizeFile();
         }
 
+        private void Show_data()
+        {
+            Write_date.Content = csvPickedFile.write_date;
+            Main_analize_page.Visibility = Visibility.Visible;
+
+            Write_time.Content = csvPickedFile.write_time;
+            Work_time_percent.Content = csvPickedFile.work_time_percent.ToString() + '%';
+            Laser_work_time_percent.Content = csvPickedFile.laser_work_time_percent.ToString() + '%';
+
+            Work_time.Content = csvPickedFile.work_time;
+            Laser_work_time.Content = csvPickedFile.laser_work_time;
+
+            LineSeries graph_data_work = new LineSeries() { Values = new ChartValues<int>(), Title = "Work time" };
+            LineSeries graph_data_laser = new LineSeries() { Values = new ChartValues<int>(), Title = "Laser time" };
+
+            // In zero hour add zero work state
+            graph_data_laser.Values.Add(0);
+            graph_data_work.Values.Add(0);
+
+            foreach (One_record hour in csvPickedFile.graph_work_hours)
+            {
+                graph_data_laser.Values.Add(hour.Laser_state);
+                graph_data_work.Values.Add(hour.Machine_state);
+            }
+
+            WorkGraph.Series.Clear();
+            WorkGraph.Series.Add(graph_data_laser);
+            WorkGraph.Series.Add(graph_data_work);
+        }
+
+        private void Work(string fileName)
+        {
+            /*
+             * Work after choose file
+             */
+            Trace.WriteLine("-- File choosed succesful: " + fileName);
+
+            File_not_selected1.Visibility = Visibility.Hidden;
+            File_not_selected.Visibility = Visibility.Hidden;
+            File_selected.Visibility = Visibility.Visible;
+            File_name.Content = fileName;
+
+            csvPickedFile.Read_File(fileName);
+            csvPickedFile.Parse_File();
+            csvPickedFile.Analize();
+
+            Show_data();
+        }
+
         private void Select_file()
         {
-            bool? isOpened = csvPickedFile.Pick_File();
+            string fileName = ReadPickAnalizeFile.Pick_File();
 
-            if (isOpened == true)
-            {
-                Trace.WriteLine("-- File choosed succesful: " + csvPickedFile.fileName);
-
-                File_not_selected1.Visibility = Visibility.Hidden;
-                File_not_selected.Visibility = Visibility.Hidden;
-                File_selected.Visibility = Visibility.Visible;
-                File_name.Content = csvPickedFile.fileName;
-
-                csvPickedFile.Read_File();
-                csvPickedFile.Parse_File();
-                csvPickedFile.Analize();
-
-                Write_date.Content = csvPickedFile.write_date;
-                Main_analize_page.Visibility = Visibility.Visible;
-            }
+            if (fileName != null) Work(fileName);
             else Trace.WriteLine("-- File not choosed");
-
         }
 
-        private void PickFileInfoMenu_Click(object sender, RoutedEventArgs e)
+        private void PickFile_Click(object sender, RoutedEventArgs e)
         {
             Select_file();
         }
 
-        private void PickFileStartPage_Click(object sender, RoutedEventArgs e)
+        private void Drop_file(object sender, DragEventArgs e)
         {
-            Select_file();
+            File_drop.Visibility = Visibility.Hidden;
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            Trace.WriteLine("-- File dropped");
+
+            if (Path.GetExtension(files[0]).ToUpperInvariant() == ".CSV") Work(files[0]);
+            else Trace.WriteLine("-- File extension not supported!");
+        }
+
+        private void Drop_enter(object sender, DragEventArgs e)
+        {
+            File_drop.Visibility = Visibility.Visible;
+        }
+
+        private void Drop_over(object sender, DragEventArgs e)
+        {
+            File_drop.Visibility = Visibility.Hidden;
         }
     }
 }
